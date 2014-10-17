239e146da2b0de7024fa81119794113508b9f6b4
kevin <kevin.lederoff@supinfo.com>
2014-10-17 09:27:39 +0200

    fichier d'innitialisation

---
+++ initial.py
@@ -0,0 +1,17 @@
+import pygame
+from pygame.locals import *
+
+def innitialisation(fenetre):
+    vert = pygame.image.load("image/vert.png")
+    fenetre.blit(vert, (110,30))
+
+    jaune = pygame.image.load("image/jaune.png")
+    fenetre.blit(jaune, (330,30))
+
+    rouge = pygame.image.load("image/rouge.png")
+    fenetre.blit(rouge, (110,250))
+
+    bleu = pygame.image.load("image/bleu.png")
+    fenetre.blit(bleu, (330,250))
+
+    pygame.display.flip()
